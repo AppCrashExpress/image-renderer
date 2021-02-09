3911f0d1295abbe77e1abaeb2d0e4f1645029858
AppCrashExpress <efimov.s2000@gmail.com>
2021-02-09 23:32:09 +0300

    Improve 3D vector class

--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,9 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <cmath>
+#include <stdexcept>
+
 template <typename T>
 inline short sign(T value) {
     return (T(0) < value) - (T(0) > value);
@@ -18,38 +21,75 @@ template <typename T>
 class Vec3 {
 public:
     Vec3() {
-        values_[0] = 0;
-        values_[1] = 0;
-        values_[2] = 0;
+        x = y = z = 0;
     }
     Vec3(T a, T b, T c) {
-        values_[0] = a;
-        values_[1] = b;
-        values_[2] = c;
+        x = a;
+        y = b;
+        z = c;
     }
 
     Vec3<T> operator + (const Vec3<T>& other) const {
-        return Vec3<T>(values_[0] + other.values_[0],
-                       values_[1] + other.values_[1],
-                       values_[2] + other.values_[2]);
+        return Vec3<T>(x + other.x,
+                       y + other.y,
+                       z + other.z);
     }
 
     Vec3<T> operator - (const Vec3<T>& other) const {
-        return Vec3<T>(values_[0] - other.values_[0],
-                       values_[1] - other.values_[1],
-                       values_[2] - other.values_[2]);
+        return Vec3<T>(x - other.x,
+                       y - other.y,
+                       z - other.z);
     }
 
     T& operator [] (int i) {
-        return values_[i];
+        switch (i) {
+          case 0:
+            return x;
+          case 1:
+            return y;
+          case 2:
+            return z;
+          default:
+            throw std::out_of_range("Vec3::range_more_than_2");
+        }
     }
 
     const T& operator [] (int i) const {
-        return values_[i];
+        switch (i) {
+          case 0:
+            return x;
+          case 1:
+            return y;
+          case 2:
+            return z;
+          default:
+            throw std::out_of_range("Vec3::range_more_than_2");
+        }
+    }
+
+    float length() {
+        return sqrt(dot_product(*this, *this));
+    }
+
+    void normalize() {
+        float vec_length = length();
+        x /= vec_length;
+        y /= vec_length;
+        z /= vec_length;
+    }
+
+    inline static float dot_product(const Vec3<T>& a, const Vec3<T>& b) {
+        return a.x * b.x + a.y * b.y + a.z * b.z;
+    }
+
+    inline static Vec3<T> cross_product(const Vec3<T>& a, const Vec3<T>& b) {
+        return Vec3<T>(a.y * b.z - a.z * b.y,
+                       a.z * b.x - a.x * b.z,
+                       a.x * b.y - a.y * b.x);
     }
 
 private: 
-    T values_[3];
+    T x, y, z;
 };
 
 using Vec3i = Vec3<int>;
