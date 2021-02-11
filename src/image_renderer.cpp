fbb3fafbfb90babe5c9b35933868032ca5b53369
AppCrashExpress <efimov.s2000@gmail.com>
2021-02-11 18:44:05 +0300

    Add rendering prototype

--- src/image_renderer.cpp
+++ src/image_renderer.cpp
@@ -11,21 +11,43 @@ ImageRenderer::ImageRenderer() {
 ImageRenderer::ImageRenderer(int width, int height, int bpp)
     : ImageRenderer() {
     image_ = TGAImage(width, height, bpp);
+    
+    image_width_  = width;
+    image_height_ = height;
 }
 
 ImageRenderer::ImageRenderer(TGAImage& image)
     : ImageRenderer() {
     image_ = image;
+
+    image_width_  = image.get_width();
+    image_height_ = image.get_height();
 }
 
 ImageRenderer::ImageRenderer(TGAImage&& image)
     : ImageRenderer() {
     image_ = image;
+
+    image_width_  = image.get_width();
+    image_height_ = image.get_height();
 }
 
 void ImageRenderer::draw(const Model& model) {
     const std::vector<Vec3d>& vertices = model.get_verts();
     const std::vector<Vec3i>& faces    = model.get_face_verts();
+
+//    const std::vector<Vec3d>& vertices = {
+//        { 0.0f, -0.5f,  0.0f},
+//        { 0.5f,  0.5f,  0.5f},
+//        {-0.5f,  0.5f,  0.5f},
+//        {-0.5f,  0.5f, -0.5f},
+//        { 0.5f,  0.5f, -0.5f},
+//    };
+//    const std::vector<Vec3i>& faces = {
+//        {0, 1, 3},
+//        {0, 4, 2}
+//    };
+
 
     for (const auto& face : faces) {
         Vec3d vert_a = vertices[ face[0] ];
@@ -44,10 +66,7 @@ void ImageRenderer::draw(const Model& model) {
 
         float lights_white = 255 * intensity;
 
-        fill_triangle(to_screen_coords(vert_a),
-                      to_screen_coords(vert_b),
-                      to_screen_coords(vert_c),
-                      image_,
+        fill_triangle(vert_a, vert_b, vert_c,
                       TGAColor(lights_white, lights_white, lights_white, 255) );
     }
 
@@ -66,9 +85,49 @@ bool ImageRenderer::check_visible(const Vec3d& norm) {
     return Vec3d::dot_product(norm, cam_dir_) > 0;
 }
 
-Point ImageRenderer::to_screen_coords(const Vec3d& vert) {
+ImageRenderer::ScreenPoint
+ImageRenderer::to_screen_coords(const Vec3d& vert) {
     int x = (vert[0] + 1.0f) * image_.get_width() / 2;
     int y = (vert[1] + 1.0f) * image_.get_height() / 2;
-    return { x, y };
+    float z = vert[2];
+    return { x, y, z };
 }
 
+Vec3d ImageRenderer::convert_barycentric(const ScreenPoint& p_a,
+                                         const ScreenPoint& p_b,
+                                         const ScreenPoint& p_c,
+                                         const ScreenPoint& p_current) {
+    const Vec3d x_side = Vec3d(p_c.x - p_a.x,
+                               p_b.x - p_a.x,
+                               p_a.x - p_current.x);
+    const Vec3d y_side = Vec3d(p_c.y - p_a.y,
+                               p_b.y - p_a.y,
+                               p_a.y - p_current.y);
+    const Vec3d params = Vec3d::cross_product(x_side, y_side);
+    
+    if (std::abs(params[2]) < 1.0f) { Vec3d(-1, -1, -1); }
+    return Vec3d(1.0f - (params[0] + params[1]) / params[2],
+                 params[1] / params[2],
+                 params[0] / params[2]);
+}
+
+void ImageRenderer::fill_triangle(const Vec3d& vert_a,
+                                  const Vec3d& vert_b,
+                                  const Vec3d& vert_c,
+                                  const TGAColor& color) {
+    ScreenPoint p_a = to_screen_coords(vert_a);
+    ScreenPoint p_b = to_screen_coords(vert_b);
+    ScreenPoint p_c = to_screen_coords(vert_c);
+
+    for (int w = 0; w < image_width_; ++w) {
+        for (int h = 0; h < image_height_; ++h) {
+            ScreenPoint current = {w, h, 0};
+            Vec3d params = convert_barycentric(p_a, p_b, p_c, current);
+            
+            if (params[0] < 0 || params[1] < 0 || params[2] < 0) {
+                continue;
+            }
+            image_.set(w, h, color);
+        }
+    }
+}
--- src/image_renderer.h
+++ src/image_renderer.h
@@ -19,10 +19,42 @@ public:
 private:
     ImageRenderer();
 
+    struct ScreenPoint {
+        int x, y;
+        float z;
+
+        ScreenPoint operator + (const ScreenPoint& other) const {
+            return ScreenPoint({
+                        x + other.x,
+                        y + other.y,
+                        z + other.z
+                    });
+        }
+
+        ScreenPoint operator * (float val) const {
+            int   new_x = x * val;
+            int   new_y = y * val;
+            float new_z = z * val;
+            return ScreenPoint( {new_x, new_y, new_z} );
+        }
+    };
+
     bool check_visible(const Vec3d& norm);
-    Point to_screen_coords(const Vec3d& vert);
+    ScreenPoint to_screen_coords(const Vec3d& vert);
+
+    Vec3d convert_barycentric(const ScreenPoint& p_a,
+                              const ScreenPoint& p_b,
+                              const ScreenPoint& p_c,
+                              const ScreenPoint& p_current);
+    void fill_triangle(const Vec3d& vert_a,
+                       const Vec3d& vert_b,
+                       const Vec3d& vert_c,
+                       const TGAColor& color);
 
     TGAImage image_;
+    int image_height_;
+    int image_width_;
+
     Vec3d cam_dir_;
     Vec3d light_dir_;
 };
